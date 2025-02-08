4dfa95304f359bbcdb4a3751d5729b3750a9a286
shofiq6030 <mdshofiq6030@gmail.com>
2025-02-09 00:43:15 +0600

    add initial project setup

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const router = require('./src/routes/api')
+const app = express()
+const bodyParser = require('body-parser')
+require('dotenv').config();
+
+
+//Security Midduleware
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+const mongoSanitize = require('express-mongo-sanitize')
+const hpp = require('hpp')
+const cors = require('cors')
+
+
+
+//Security Midduleware Implement
+app.use(cors())
+app.use(helmet())
+app.use(mongoSanitize())
+app.use(hpp())
+
+// BodyParser 
+app.use(bodyParser.json())
+
+//Rate Limiter 
+const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 3000 })
+app.use(limiter)
+
+// Database
+const mongoose = require('mongoose')
+mongoose
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+
+        console.log("DB Connected");
+    })
+    .catch((err) => console.log(err));
+
+
+// Frontend Tagging (If needed)
+// app.use(express.static('client/dist'))
+// app.get("*", function (req, res) {
+//     res.sendFile(__dirname + '/client/dist/index.html');
+// });
+
+// Managing BackEnd API Routing
+app.use("/api/v1", router)
+
+module.exports = app
---
+++ src/routes/api.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+
+
+router.get("/", async (req, res) => {
+    try {
+        res.status(200).json("hello");
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+
+
+module.exports = router;
