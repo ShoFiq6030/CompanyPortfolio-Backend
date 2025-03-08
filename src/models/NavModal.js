94bfcecf8ffc885f1b72d209db809676cbb3458d
shofiq6030 <mdshofiq6030@gmail.com>
2025-03-08 22:21:21 +0600

    modify navbar model

--- src/models/NavModal.js
+++ src/models/NavModal.js
@@ -1,21 +1,19 @@
 const mongoose = require("mongoose");
+
 const dataSchema = new mongoose.Schema(
     {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-        },
         logoUrl: {
             type: String,
             default: 'https://res.cloudinary.com/dutnq2gdm/image/upload/v1739460894/D.E.M.O._Logo_2006_b2zhgh.svg',
         },
-        ourCompanies: {
-            type: [String],
-            required: true,
-        },
-        products: {
-            type: [String],
-            required: true,
-        },
+        ourCompanies: [{
+            name: String,
+            url: String,
+        }],
+        products: [{
+            name: String,
+            url: String,
+        }],
     },
     { timestamps: true, versionKey: false }
 );
